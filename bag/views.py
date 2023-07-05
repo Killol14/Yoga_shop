7616f40979f2f266e631857b53a77b57b8604ea0
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-05 10:55:09 +0000

    bug for terminal fixed

--- bag/views.py
+++ bag/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render, redirect
 def view_bag(request):
     return render(request, 'bag/bag.html')
 
-def add_to_bag(request, iteam_id):
+def add_to_bag(request, item_id):
 
-    quntity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
@@ -17,4 +17,5 @@ def add_to_bag(request, iteam_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    return redirect(redirect_url)    
+    print(request.session['bag'])
+    return redirect(redirect_url)   
