86c96a029f3781a2356406c847f77505bfa78f13
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-08 13:36:10 +0000

    commiting for size function

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,9 +1,7 @@
-
-
 from decimal import Decimal
 from django.conf import settings
-from djanago.shortcuts import get_object_or_404
-from product.models import Product
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
@@ -12,7 +10,7 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-       for item_id, item_data in bag.items():
+    for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
