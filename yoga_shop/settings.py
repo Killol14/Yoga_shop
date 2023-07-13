86ee5d7350748dd6ceeec4d38d94b5cfde310437
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-13 12:58:31 +0000

    commiting for database in setting.py

--- yoga_shop/settings.py
+++ yoga_shop/settings.py
@@ -127,8 +127,9 @@ WSGI_APPLICATION = 'yoga_shop.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse9os(os.environ.get('DATABASE_URL'))
-    }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+   }
+
 else:
     DATABASES = {
         'default': {
