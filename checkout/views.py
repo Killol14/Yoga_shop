7102710271f0008e8c4a50b8469917ec0335fc18
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-15 14:45:44 +0000

    added stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings 
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -8,12 +9,25 @@ from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your cart for Now!")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+     stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+        )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
