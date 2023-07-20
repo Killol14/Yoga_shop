a9539be50a1592568af72ad33122916a7dfb106b
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-20 11:31:01 +0000

    amended color choice in bag.html

--- bag/views.py
+++ bag/views.py
@@ -8,38 +8,61 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
-def add_to_bag(request, item_id):
-    """ Adding Item"""
+# views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from django.contrib import messages
 
+def add_to_bag(request, item_id):
+    """ Adding Item to Bag """
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
+
+    color = None
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
+
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in list(bag.keys()):
+        if item_id in bag.keys():
             if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'You Have updated a size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                bag[item_id]['items_by_size'][size]['quantity'] += quantity
+                messages.success(request, f'You have updated the size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]["quantity"]}')
             else:
-                bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'You Have Added New Size {size.upper()} {product.name} to your Shopping Cart.')
+                bag[item_id]['items_by_size'][size] = {
+                    'quantity': quantity,
+                    'color': color  # Add the selected color to the bag
+                }
+                messages.success(request, f'You have added a new size {size.upper()} {product.name} to your shopping cart.')
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'You Have Added New Size {size.upper()} {product.name} to your Shopping Cart.')
+            bag[item_id] = {
+                'items_by_size': {
+                    size: {
+                        'quantity': quantity,
+                        'color': color  # Add the selected color to the bag
+                    }
+                }
+            }
+            messages.success(request, f'You have added a new size {size.upper()} {product.name} to your shopping cart.')
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-            messages.success(request, f'You Have Updated {product.name} the quantity to {bag[item_id]}')
+        if item_id in bag.keys():
+            bag[item_id]['quantity'] += quantity
+            messages.success(request, f'You have updated {product.name} quantity to {bag[item_id]["quantity"]}')
         else:
-            bag[item_id] = quantity
-            messages.success(request, f'You Have Added {product.name} to your Shopping Cart.')
+            bag[item_id] = {
+                'quantity': quantity,
+                'color': color  # Add the selected color to the bag
+            }
+            messages.success(request, f'You have added {product.name} to your shopping cart.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
 
 def adjust_bag(request, item_id):
     """ Adjust Item """
