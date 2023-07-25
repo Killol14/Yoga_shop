8ccd78d3ac7bc2db545f756799fccf2b453818e2
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-25 11:54:32 +0000

    commiting for bug fix for update items

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def bag_contents(request):
 
+
+def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
@@ -21,47 +22,44 @@ def bag_contents(request):
                 'product': product,
             })
         else:
-          product = get_object_or_404(Product, pk=item_id)
+            product = get_object_or_404(Product, pk=item_id)
 
-    if 'items_by_size' in item_data:  # Check if 'items_by_size' key exists
-        for size, colors_data in item_data['items_by_size'].items():
-            for color, quantity_str in colors_data.items():
-                try:
-                    # Convert quantity to an integer
-                    quantity = int(quantity_str)
-                except ValueError:
-                    # Handle the case when quantity_str is not a valid integer
-                    # You can choose to ignore, log, or handle the error as needed.
-                    continue
-
-                # Perform calculations
-                total += quantity * product.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                    'color': color,
-                })
-
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
-
-    grand_total = delivery + total
-
-    context = {
-        'bag_items': bag_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
-    }
+            if 'items_by_size' in item_data:  
+                for size, colors_data in item_data['items_by_size'].items():
+                    for color, quantity_str in colors_data.items():
+                        try:
+                            quantity = int(quantity_str)
+                        except ValueError:
+                        
+                            continue
+                        total += quantity * product.price
+                        product_count += quantity
+                        bag_items.append({
+                            'item_id': item_id,
+                            'quantity': quantity,
+                            'product': product,
+                            'size': size,
+                            'color': color,
+                        })
+        if total < settings.FREE_DELIVERY_THRESHOLD:
+            delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+        else:
+             delivery = 0
+             free_delivery_delta = 0
+                
+        grand_total = delivery + total
+                
+        context = {
+             'bag_items': bag_items,
+             'total': total,
+             'product_count': product_count,
+             'delivery': delivery,
+             'free_delivery_delta': free_delivery_delta,
+             'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+             'grand_total': grand_total,
+                }
 
     return context
+
+
--- bag/views.py
+++ bag/views.py
@@ -3,15 +3,13 @@ from django.contrib import messages
 
 from products.models import Product
 
+from .contexts import bag_contents
+
 def view_bag(request):
-    """ A view that renders the bag contents page """
+    context = bag_contents(request) 
 
     return render(request, 'bag/bag.html')
 
-# views.py
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product
-from django.contrib import messages
 
 def add_to_bag(request, item_id):
     """ Adding Item to Bag """
@@ -63,36 +61,65 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
-
 def adjust_bag(request, item_id):
-    """ Adjust Item """
+    """Adjust Item"""
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
+    color = request.POST.get('product_color')
     bag = request.session.get('bag', {})
 
-    if size:
-        if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'You Have updated a size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+    if size and color:
+        if item_id in bag.keys():
+            if size in bag[item_id]['items_by_size'].keys():
+                if color in bag[item_id]['items_by_size'][size]['colors'].keys():
+                    bag[item_id]['items_by_size'][size]['colors'][color] += quantity
+                    messages.success(request, f'You have updated the {color} {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]["colors"][color]}')
+                else:
+                    bag[item_id]['items_by_size'][size]['colors'][color] = quantity
+                    messages.success(request, f'You have added a new {color} {size.upper()} {product.name} to your shopping cart.')
+            else:
+                bag[item_id]['items_by_size'][size] = {
+                    'colors': {
+                        color: quantity
+                    }
+                }
+                messages.success(request, f'You have added a new {color} {size.upper()} {product.name} to your shopping cart.')
         else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-                messages.success(request, f'You Have Removed Size {size.upper()} {product.name} from your Shopping Cart.')
+            bag[item_id] = {
+                'items_by_size': {
+                    size: {
+                        'colors': {
+                            color: quantity
+                        }
+                    }
+                }
+            }
+            messages.success(request, f'You have added a new {color} {size.upper()} {product.name} to your shopping cart.')
+    elif size:
+        messages.warning(request, 'Please select a color for the product.')
+        
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(request, f'You Have Updated {product.name} the quantity to {bag[item_id]}')
+        quantity = request.POST.get('quantity')
+        if quantity is not None:
+            try:
+                quantity = int(quantity)
+                if quantity > 0:
+                    bag[item_id] = quantity
+                    messages.success(request, f'You have updated {product.name} quantity to {bag[item_id]}')
+                else:
+                    bag.pop(item_id)
+                    messages.success(request, f'You have removed the item {product.name} from your shopping cart.')
+            except ValueError:
+                messages.error(request, 'Invalid quantity value.')
         else:
-            bag.pop(item_id)
-            messages.success(request, f'You Have Removed the item {product.name} from your Shopping Cart.')
+            messages.error(request, 'Quantity not provided.')
+        
 
     request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+    return redirect('view_bag')
 
 def remove_from_bag(request, item_id):
     """Remove the"""
