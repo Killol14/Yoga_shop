cd1c6ca28a913417bbdced6e04bff22b8eacb4b5
Killol14 <kgselectrical.plumbing@gmail.com>
2023-07-31 17:18:40 +0000

    DEBUG True

--- Yoga_shop/settings.py
+++ Yoga_shop/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-killol14-yogashop-skf6kb1nlay.ws-eu102.gitpod.io'] 
 
